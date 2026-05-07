c1db235b4ab6321a5ce323e2868f48296f2efcc5
Raafx <ahmadrafiin467@gmail.com>
2026-05-07 15:28:41 +0800

    FIX BUG FITUR ADMIN

--- Program_Utama/Program_header/Admin_login.cpp
+++ Program_Utama/Program_header/Admin_login.cpp
@@ -8,49 +8,66 @@
 using namespace std;
 using json = nlohmann::json;
 
-const string FILE_ADMIN = "../Data_json/Data_admin.json"; 
+// Mengubah Path json
+const string FILE_ADMIN = "Data_json/Data_admin.json";
 
-bool login_admin(){
+bool login_admin()
+{
     string nama, pass;
     int batas_login = 0;
 
-    while(batas_login < 3){
+    while (batas_login < 3)
+    {
         system("cls");
         cout << "=======================" << endl;
         cout << "===== LOGIN ADMIN =====" << endl;
         cout << "=======================" << endl;
-        cout << "Username : "; 
+        cout << "Username : ";
         cin >> nama;
-        cout << "Password : "; 
+        cout << "Password : ";
         cin >> pass;
 
         json j;
         ifstream fileIn(FILE_ADMIN);
 
-        if(fileIn.is_open()){
-            if(fileIn.peek() != std::ifstream::traits_type::eof()){
+        // Menambah pesan status file
+        if (!fileIn)
+        {
+            cout << "FILE GAGAL DIBUKA!" << endl;
+            return false;
+        }
+
+        if (fileIn.is_open())
+        {
+
+            if (fileIn.peek() != std::ifstream::traits_type::eof())
+            {
                 fileIn >> j;
             }
             fileIn.close();
 
-            for(const auto& item : j){
-                if(item["username"] == nama && item["password"] == pass) {
+            for (const auto &item : j)
+            {
+                if (item["username"] == nama && item["password"] == pass)
+                {
                     cout << "[BERHASIL] Login Berhasil! Selamat Datang, Juragan " << nama << "!" << endl;
                     system("pause");
                     system("cls");
                     return true;
                 }
             }
-        } 
+        }
 
         batas_login++;
         int sisa_percobaan = 3 - batas_login;
 
-        if(batas_login < 3){
+        if (batas_login < 3)
+        {
             cout << "[GAGAL] Username atau Password Admin Salah! (Sisa Percobaan: " << sisa_percobaan << "x )" << endl;
             system("pause");
-        } 
-        else{
+        }
+        else
+        {
             cout << "[KACAU] Anda Telah Salah 3x. Kembali ke Menu Utama!!!" << endl;
             system("pause");
             system("cls");
@@ -61,11 +78,13 @@ bool login_admin(){
     return false;
 }
 
-void menu_admin(){
+void menu_admin()
+{
     int pilihan;
     bool admin_menu = true;
 
-    while(admin_menu){
+    while (admin_menu)
+    {
         system("cls");
 
         cout << "======================================" << endl;
@@ -79,29 +98,31 @@ void menu_admin(){
         cout << "Pilihan: ";
         cin >> pilihan;
 
-        switch(pilihan){
-            case 1:
-                break;
+        switch (pilihan)
+        {
+        case 1:
+            break;
 
-            case 2:
-                break;
+        case 2:
+            break;
 
-            case 3:
-                break;
+        case 3:
+            break;
 
-            case 4:
-                break;
+        case 4:
+            break;
 
-            case 5:
-                admin_menu = false;
-                system("pause");
-                system("cls");
-                break;
+        case 5:
+            admin_menu = false;
+            system("pause");
+            system("cls");
+            break;
 
-            default:
-                cout << "[ERROR] Pilihan Tidak Valid, Silahkan Input Pilihan Yang Sesuai!!!" << endl;
-                system("pause");
-                system("cls");
-                break; 
+        default:
+            cout << "[ERROR] Pilihan Tidak Valid, Silahkan Input Pilihan Yang Sesuai!!!" << endl;
+            system("pause");
+            system("cls");
+            break;
         }
+    }
 }
